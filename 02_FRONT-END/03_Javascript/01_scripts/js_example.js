91afdb77121f13cef80bf7c77688a7e676d022e0
Guilherme Dias <guidias1212@users.noreply.github.com>
2019-09-17 22:58:52 -0300

    Added Javascript examples
    Added a few more Javascript examples to code.

--- 02_FRONT-END/03_Javascript/01_scripts/js_example.js
+++ 02_FRONT-END/03_Javascript/01_scripts/js_example.js
@@ -1,4 +1,53 @@
 /* External Javascript */
 function myFunction3() {
- document.getElementById("demo3").innerHTML = "Paragraph changed.";
+	document.getElementById("demo3").innerHTML = "Paragraph changed.";
+}
+
+function myFunction4() {
+	document.write(5 + 6);
+}
+
+function myFunction5() {
+	window.alert(5 + 6);
+}
+
+function myFunction6() {
+	console.log(5 + 6);
+}
+
+function myFunction7() {
+	var x, y, z;       // How to declare variables
+	x = 5; y = 6;      // How to assign values
+	z = x + y;         // How to compute values
+	window.alert(z);
+}
+
+function myFunction8() {     
+	window.alert(5 + "6");
+}
+
+function myFunction9() {     
+	window.alert("5" + 6);
+}
+
+function myFunction10() {  
+	var person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+	window.alert(person);
+}
+
+function myFunction11(a,b) {  
+	window.alert(a + b);
+}
+
+var user = {
+		  firstName: "John",
+		  lastName : "Doe",
+		  id       : 5566,
+		  fullName : function() {
+		    return this.firstName + " " + this.lastName;
+		  }
+		};
+
+function myFunction12(a_user) {  
+	window.alert(a_user.fullName());
 }
